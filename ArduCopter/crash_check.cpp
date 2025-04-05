8513495b184db63b0091b869f0fe10496abe76d1
Oleksandr Sytnyk <oleksandr.v.sytnyk@gmail.com>
2025-04-05 18:59:55 +0000

    Added counter reset to thrust imbalance check in case thrust balance goes back to normal before threshold.

--- ArduCopter/crash_check.cpp
+++ ArduCopter/crash_check.cpp
@@ -237,7 +237,10 @@ void Copter::thrust_imbalance_check()
         }
         mask = mask>>1;
     }
+    
+    // reset counter if thrust balance goes back to norm
     if (min_thrust < FLT_EPSILON || max_thrust/min_thrust < THRUST_IMBALANCE_THRESHOLD) {
+        thrust_imbalance_counter = 0;
         return;
     }
     
